f032d5fcb3383c19edc8c9b5d151447fb41d9c4b
Sébastien Seigneur <seigneur.sebastien.sio@gmail.com>
2022-09-01 00:03:04 +0200

    :sparkles: :construction: responsive menu and toggle

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,13 @@
 <template>
   <header>
-    <ul class="container">
-      <li><g-link to="/">Home</g-link></li>
-      <li><g-link to="/about/">A propos de moi</g-link></li>
-      <li><g-link href="#">Mes projets</g-link></li>
-      <li><g-link href="#">Contact</g-link></li>
-    </ul>
+    <nav>
+      <ul>
+        <li><g-link to="/">Home</g-link></li>
+        <li><g-link to="/about/">A propos de moi</g-link></li>
+        <li><g-link href="#">Mes projets</g-link></li>
+        <li><g-link href="#">Contact</g-link></li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -15,40 +17,65 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
 header {
-  position: absolute;
+  position: fixed;
   height: 70px;
   width: 100%;
-  background-color: #29456F;
+  background-color: #294570;
   z-index: 100;
+  transition: .7s;
+
+  nav, ul {
+    height: 100%;
+  }
+
+  ul {
+    width: 1140px;
+    list-style-type: none;
+    display: flex;
+    align-items: center;
+    margin: auto;
+    padding: 0;
+    overflow: hidden;
+
+    li a {
+      display: block;
+      color: #FFEDCB;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 18px;
+
+      &:hover {
+        color: white;
+      }
+    }
+  }
 }
 
-ul {
-  list-style-type: none;
-  display: flex;
-  height: 100%;
-  align-items: center;
-  margin: auto;
-  padding: 0;
-  overflow: hidden;
+@media (max-width:$mobile-breakpoint) {
+  header {
+    height: 100%;
+    mix-blend-mode: luminosity;
+    backdrop-filter: blur(20px);
+    background-color: hsla(0, 0%, 46%, 0.3);
+
+    ul {
+      width: unset;
+      flex-direction: column;
+      align-items: flex-start;
+      // used unquote to circumvent Sass compile error : 'Incompatible units', may replace later
+      padding: unquote("min(50px, 15vh)");
+
+      li a {
+        text-shadow: 1px 1px 3px black;
+        font-size: 22px;
+        color: hsl(39, 100%, 90%);
+      }
+    }
+  }
 }
 
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: #FFEDCB;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 18px;
-}
-
-li a:hover {
-  color: white;
-}
 </style>
