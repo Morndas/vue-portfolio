1c6cc9a824a89d693e0d5d263214465d7aab60b0
Sébastien Seigneur <seigneur.sebastien.sio@gmail.com>
2022-09-19 02:18:18 +0200

    :construction: about page content placeholder

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,23 @@
 <template>
   <ContentLayout>
-    <template v-slot:main-body>
+    <template v-slot:main-content>
+      <div class="about-me PLACEHOLDER">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium amet animi asperiores cum ducimus eaque,
+          inventore itaque iusto maiores, maxime molestias necessitatibus, non pariatur quia rerum soluta suscipit
+          tenetur!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium amet animi asperiores cum ducimus eaque,
+          inventore itaque iusto maiores, maxime molestias necessitatibus, non pariatur quia rerum soluta suscipit
+          tenetur! maxime molestias necessitatibus, non pariatur quia rerum soluta suscipit
+          tenetur!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusantium amet animi asperiores cum ducimus eaque,
+          inventore itaque iusto maiores.
+        </p>
+      </div>
     </template>
   </ContentLayout>
 </template>
@@ -13,3 +30,19 @@ export default {
   }
 }
 </script>
+
+<style scoped lang="scss">
+
+.about-me.PLACEHOLDER {
+  text-align: center;
+
+  p {
+    background: rgba(50, 205, 50, 0.88);
+    border: 1px solid black;
+    padding: 30px;
+    max-width: 70%;
+    margin: 20px auto;
+    border-radius: 30px;
+  }
+}
+</style>
